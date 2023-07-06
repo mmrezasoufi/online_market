38fd66022ca8b66a2f94e2ffd0525b19012537e8
mmrezasoufi <mrsoofi9@gmail.com>
2023-07-07 02:20:37 +0330

    handle SECRET_KEY and DEBUG

--- puddle/settings.py
+++ puddle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from puddle.local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k=ze2hb5grv0^-=-ty0**0i*4_xax+757a2&=if)6ad1h(dj@k'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
