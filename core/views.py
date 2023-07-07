c0aba798a797e428a66213651837cec1dd2ecf4e
mmrezasoufi <mrsoofi9@gmail.com>
2023-07-07 18:46:49 +0330

    signup and login (front and back)

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from items.models import Category, Item
 
+from core.forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -14,3 +15,19 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
--- puddle/urls.py
+++ puddle/urls.py
@@ -3,11 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.views import index, contact
 
 urlpatterns = [
     path('items/', include("items.urls")),
-    path('contact/', contact, name='contact'),
-    path('', index, name='index'),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
