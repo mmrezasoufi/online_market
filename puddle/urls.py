a41c29fb1d1f860307216bbd9096d5a179ebc124
mmrezasoufi <mrsoofi9@gmail.com>
2023-07-07 17:44:34 +0330

    add detail url

--- puddle/urls.py
+++ puddle/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 from core.views import index, contact
 
 urlpatterns = [
+    path('items/', include("items.urls")),
     path('contact/', contact, name='contact'),
     path('', index, name='index'),
     path('admin/', admin.site.urls),
