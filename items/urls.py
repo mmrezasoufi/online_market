fb5788dfd74f880f731d68ff1d22116d88b5d8d5
mmrezasoufi <mrsoofi9@gmail.com>
2023-07-09 22:23:10 +0330

    edit items feature

--- items/urls.py
+++ items/urls.py
@@ -7,5 +7,6 @@ app_name = 'items'
 urlpatterns = [
     path('new/', new, name='new'),
     path('<int:pk>/', detail, name='detail'),
-    path('<int:pk>/delete/', delete, name='delete')
+    path('<int:pk>/delete/', delete, name='delete'),
+    path('<int:pk>/edit/', edit, name='edit')
 ]
--- items/views.py
+++ items/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from items.models import Item
 from django.contrib.auth.decorators import login_required
-from items.forms import NewItemForm
+from items.forms import NewItemForm, EditItemForm
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -33,6 +33,24 @@ def new(request):
         'title': 'New item'
     })
 
+
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+        
+        if form.is_valid():
+            form.save()
+
+            return redirect('items:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    
+    return render(request, 'items/form.html', {
+        'form': form,
+        'title': 'Edit item'
+    })
 
 @login_required
 def delete(request, pk):
